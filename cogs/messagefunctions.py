e1ddc666efb49da7890c35f55c6164387e066d16
evanpurl <evan@purlcomputers.com>
2023-03-15 20:32:49 -0600

    testing

--- cogs/messagefunctions.py
+++ cogs/messagefunctions.py
@@ -14,28 +14,31 @@ class messagefunctions(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        tagged = message.mentions
-        if tagged[0].id == self.bot.user.id:
-            msg = message.content.lower().translate(str.maketrans('', '', string.punctuation)).replace(self.bot.user.id, "").split(" ")
-            print(msg)
-            answer = await getanswer(msg)
-            response = await getgreeting(msg)
-            ily = await getily(msg)
-            compliment = await getcompliment(msg)
-            if answer:
-                await message.reply(f"{answer}!")
-            if response:
-                await message.reply(f"{response} {message.author.name}!")
-            elif ily:
-                await message.reply(f"{ily} {message.author.name}!")
-            elif compliment:
-                await message.reply(f"{compliment} {message.author.name}!")
-            await asyncio.sleep(3)
+        try:
+            tagged = message.mentions
+            if tagged[0].id == self.bot.user.id:
+                msg = message.content.lower().translate(str.maketrans('', '', string.punctuation)).replace(self.bot.user.id, "").split(" ")
+                print(msg)
+                answer = await getanswer(msg)
+                response = await getgreeting(msg)
+                ily = await getily(msg)
+                compliment = await getcompliment(msg)
+                if answer:
+                    await message.reply(f"{answer}!")
+                if response:
+                    await message.reply(f"{response} {message.author.name}!")
+                elif ily:
+                    await message.reply(f"{ily} {message.author.name}!")
+                elif compliment:
+                    await message.reply(f"{compliment} {message.author.name}!")
+                await asyncio.sleep(3)
 
-        if message.author == self.bot.user:  # If message is from itself, do nothing
-            return
-        if message.author.bot:  # If message is a bot, do nothing
-            return
+            if message.author == self.bot.user:  # If message is from itself, do nothing
+                return
+            if message.author.bot:  # If message is a bot, do nothing
+                return
+        except Exception as e:
+            print(e)
 
 
 async def setup(bot):
