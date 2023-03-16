f2fd5bb64984488967e58ef710ae73c647676bff
evanpurl <evan@purlcomputers.com>
2023-03-15 20:36:27 -0600

    testing

--- cogs/messagefunctions.py
+++ cogs/messagefunctions.py
@@ -19,6 +19,7 @@ class messagefunctions(commands.Cog):
             if tagged[0].id == self.bot.user.id:
                 msg = message.content.lower().translate(str.maketrans('', '', string.punctuation)).replace(str(self.bot.user.id), "").split(" ")
                 msg = " ".join(msg)
+                print(msg)
                 answer = await getanswer(msg)
                 response = await getgreeting(msg)
                 ily = await getily(msg)
