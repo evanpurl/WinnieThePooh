03b839e98349094de072abeae181633ed8cc6afe
evanpurl <evan@purlcomputers.com>
2023-03-15 19:57:15 -0600

    Added more wake words for Winnie

--- cogs/messagefunctions.py
+++ cogs/messagefunctions.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 from database.database import getanswer, getgreeting, getily, getcompliment
 import string
 
+triggerwords = ["winnie", "pooh bear", "pooh", "winnie the pooh"]
+
 
 class messagefunctions(commands.Cog):
 
@@ -18,22 +20,23 @@ class messagefunctions(commands.Cog):
             return
         msg = message.content.lower().translate(str.maketrans('', '', string.punctuation)).split(" ")
         for substring in msg:
-            if substring == "winnie":  # Trigger word
-                msg.remove(substring)
-                msg = " ".join(msg)
-                answer = await getanswer(msg)
-                response = await getgreeting(msg)
-                ily = await getily(msg)
-                compliment = await getcompliment(msg)
-                if answer:
-                    await message.reply(f"{answer}!")
-                if response:
-                    await message.reply(f"{response} {message.author.name}!")
-                elif ily:
-                    await message.reply(f"{ily} {message.author.name}!")
-                elif compliment:
-                    await message.reply(f"{compliment} {message.author.name}!")
-                await asyncio.sleep(3)
+            for i in triggerwords:
+                if substring == i:
+                    msg.remove(substring)
+                    msg = " ".join(msg)
+                    answer = await getanswer(msg)
+                    response = await getgreeting(msg)
+                    ily = await getily(msg)
+                    compliment = await getcompliment(msg)
+                    if answer:
+                        await message.reply(f"{answer}!")
+                    if response:
+                        await message.reply(f"{response} {message.author.name}!")
+                    elif ily:
+                        await message.reply(f"{ily} {message.author.name}!")
+                    elif compliment:
+                        await message.reply(f"{compliment} {message.author.name}!")
+                    await asyncio.sleep(3)
 
 
 async def setup(bot):
